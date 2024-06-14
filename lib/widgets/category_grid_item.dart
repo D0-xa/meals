36a8f7e6334abd1ef1cbe9293b4421642a0a9ecb
D0-xa <emmanuelchidie@gmail.com>
2024-06-14 19:29:51 +0100

    The coding journey has begone

---
+++ lib/widgets/category_grid_item.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+import 'package:meals/models/category.dart';
+
+class CategoryGridItem extends StatelessWidget {
+  const CategoryGridItem({
+    super.key,
+    required this.category,
+    required this.onSelectCategory,
+  });
+
+  final Category category;
+  final void Function() onSelectCategory;
+
+  @override
+  Widget build(BuildContext context) {
+    final itemColorScheme = ColorScheme.fromSeed(seedColor: category.color);
+    return InkWell(
+      // GestureDetector will suffice but won't a visual give feedback.
+      onTap: onSelectCategory,
+      splashColor: itemColorScheme.primaryContainer,
+      borderRadius: BorderRadius.circular(5),
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(5),
+            gradient: LinearGradient(
+              colors: [
+                category.color.withOpacity(0.5),
+                category.color.withOpacity(0.9),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            )),
+        child: Text(
+          category.title,
+          style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                color: itemColorScheme.onSurface,
+              ),
+        ),
+      ),
+    );
+  }
+}
